f9d6aa4f653b88bb5bc2aef27d2b21de9de9d926
Lubomir Bucko <bucko@foundation.sk>
2023-10-02 21:38:37 +0200

    refactored main code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import "./App.css";
-import { skSK } from "@mui/x-date-pickers/locales";
-import { sk } from "date-fns/locale";
-import SignUp from "./components/forms/InsuranceForm";
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+
+import { skSK } from '@mui/x-date-pickers/locales'
+import { sk } from 'date-fns/locale'
+import InsuranceForm from './components/forms/insurance-form/InsuranceForm'
 
 function App() {
-  return (
-    <>
-      <LocalizationProvider
-        dateAdapter={AdapterDateFns}
-        adapterLocale={sk}
-        localeText={
-          skSK.components.MuiLocalizationProvider.defaultProps.localeText
-        }
-      >
-        <SignUp />
-      </LocalizationProvider>
-    </>
-  );
+    return (
+        <LocalizationProvider
+            dateAdapter={AdapterDateFns}
+            adapterLocale={sk}
+            localeText={skSK.components.MuiLocalizationProvider.defaultProps.localeText}>
+            <InsuranceForm />
+        </LocalizationProvider>
+    )
 }
 
-export default App;
+export default App
